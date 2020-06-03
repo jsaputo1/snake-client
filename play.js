91315a05e27a566bd69331a627c2d8a494be4000
John Saputo <jsaputo1@gmail.com>
2020-06-03 17:48:51 +0000

    moved functions to client.js and imported

--- play.js
+++ play.js
@@ -1,21 +1,15 @@
-const net = require("net");
-
-/**
- * Establishes connection with the game server
- */
-const connect = function () {
-  const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: 50541,
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  return conn;
-};
-
+// play.js
+const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
+
+// console.log("Successfully connected to game server");
+// console.log("Name: JS");
+
+// interpret incoming data as text
+// conn.setEncoding("utf8");
+// conn.on("data", (data) => {
+//   console.log("Server says: ", data);
+// });
+
+// To accomplish this, inside the connect function, use the .on method on our conn object to register a "connect" handler (a callback function). In the callback, print a message for us (the player) to see - something like "Successfully connected to game server" will suffice.
